10e5c922b91a6f30ba0adbe721a326417f30f649
Suchitra Chowdhury <suchitrachowdhury219@gmail.com>
2026-01-20 21:35:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("feedbackForm").addEventListener("submit", function () {
+    document.getElementById("message").innerText =
+        "Thank you! Your feedback has been submitted successfully.";
+});
