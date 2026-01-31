d902bcac51d0990c2695b06b82870f070554a03e
Suchitra Chowdhury <suchitrachowdhury219@gmail.com>
2026-01-31 18:01:30 +0530

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("feedbackForm").addEventListener("submit", function () {
-    document.getElementById("message").innerText =
-        "Thank you! Your feedback has been submitted successfully.";
-});
